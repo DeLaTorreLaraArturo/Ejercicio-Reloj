f4cd152465748978021fab9fecd65d8e1fd9a5b7
De La Torre Lara Arturo <arturo.delato@hotmail.com>
2017-08-23 15:55:06 -0500

    Si se ingresa un numero de horas mayor a 23 o numero de minutos mayor a 59 le da el valor de 0 Imprime la hora si es menor a 10 con un "0"agregado a la cadena y minutos menor a 10 "agregado a la cadena" Imprime "00" si las horas son mayor a 23 y "00"si llega a los 60 minutos

--- Reloj.java
+++ Reloj.java
@@ -5,16 +5,110 @@ class Reloj
     
     Reloj(int horasIniciales, int minutosIniciales)
     {
-        horas = horasIniciales;
-        minutos = minutosIniciales;
+        if(horasIniciales > 23)
+        {
+            horas = 0;
+            if(minutosIniciales > 59)
+            {
+                minutos = 0;
+            }
+            else
+            {
+                minutos = minutosIniciales;
+            }
+        }
+        else
+        {
+            horas = horasIniciales;
+            if(minutosIniciales > 59)
+            {
+                minutos = 0;
+            }
+            else
+            {
+                minutos = minutosIniciales;
+            }
+            
+        }   
     }
     
     public String dimeTuTiempo()
     {
         String cadResult = "";
-        cadResult += horas;
-        cadResult += ":";
-        cadResult += minutos;
+        
+        if(horas == 0)
+        {
+            cadResult += "00";
+            cadResult += ":";
+            if(minutos == 0)
+            {
+                cadResult += "00";
+            }
+            else
+            {
+                if(minutos < 10)
+                {
+                    cadResult += "0";
+                    cadResult += minutos;
+                }
+                else
+                {
+                    cadResult += minutos;
+                }
+            }
+        }
+        else
+        {
+            if(horas < 10)
+            {
+                cadResult += "0";
+                cadResult += horas;
+                cadResult += ":";
+                
+            }
+            else
+            {
+                cadResult += horas;
+                cadResult += ":";
+            }
+            if(minutos == 0)
+            {
+                cadResult += "00";
+            }
+            else
+            {
+                if(minutos < 10)
+                {
+                    cadResult += "0";
+                    cadResult += minutos;
+                }
+                else
+                {
+                    cadResult += minutos;
+                }
+            }
+        }
         return cadResult;
+    }
+    
+    public void ticTac()
+    {
+        if(minutos == 59)
+        {
+            minutos = 0;
+            if(horas == 23)
+            {
+                horas = 0;
+            }
+            else
+            {
+                horas ++;
+            }
+            
+        }
+        else 
+        {
+            minutos++;
+        }
     }
 }
